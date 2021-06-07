eb5bd14742571b7fd67ffcb4eb57e24cd5fbc527
sol <sa@miblogic.com>
2021-06-07 16:44:20 +0200

    * checking m2 is not zero * added delay between requests * added ads counter * added firefox headers to mask scrapy presense

--- athome/pipelines.py
+++ athome/pipelines.py
@@ -15,7 +15,8 @@ import logging
 from pathlib import Path
 
 class AthomePipeline:
-    
+    new_count = 0
+
     def __init__(self):
         self.df = pd.DataFrame(columns=['href','price', 'm2', 'price_m2', 'place', 'title', 'timestamp', 'status'])
     
@@ -27,9 +28,11 @@ class AthomePipeline:
             item['timestamp'] = time_stamp.strftime("%Y-%m-%d %H:%M:%S")
             item['price'] = re.sub(r'''[^0-9]''', '', item['price'])
             item['m2'] = re.sub(r'''[^0-9\.]''', '', item['m2'])
-            item['price_m2'] = float(item['price']) / float(item['m2'])
+            if(float(item['m2']) > 0):
+                item['price_m2'] = float(item['price']) / float(item['m2'])
             item['place'] = re.findall('\s([A-Z][A-zÀ-ÿ-\(\)\s]+)$', item['title'])
             item['status'] = "N"
+            new_count += 1
             self.exporter.export_item(item)
         return item
 
@@ -56,5 +59,6 @@ class AthomePipeline:
     def close_spider(self, spider):
         self.exporter.finish_exporting()
         self.file.close()
+        logging.log(logging.INFO, f'{spider.name} new ads: {self.new_count}')
         del self.df
 
--- athome/spiders/athome_dir.py
+++ athome/spiders/athome_dir.py
@@ -1,15 +1,29 @@
 import scrapy
-import pandas as pd
 from ..items import AthomeItem
-
+from random import randrange
+from time import sleep
 
 class AthomeDirSpider(scrapy.Spider):
     name = 'athome-dir'
     allowed_domains = ['athome.lu']
     
+    request_header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-GB,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-GPC": "1"
+             }
+    
     # url filter
-    start_urls = ['https://www.athome.lu/en/srp/?tr=rent&bedrooms_max=1&price_min=800&price_max=1200&q=faee1a4a&loc=L2-luxembourg&ptypes=flat,4,7,5,6,42,32,41']
+    start_url = 'https://www.athome.lu/en/srp/?tr=rent&bedrooms_max=1&price_min=800&price_max=1200&q=faee1a4a&loc=L2-luxembourg&ptypes=flat,4,7,5,6,42,32,41'
 
+    def start_requests(self):
+        yield scrapy.Request(url=self.start_url, callback=self.parse, headers=self.request_header) #, meta={"proxy": "http://192.168.44.137:8080"})
+        
     def parse(self, response):
         item = AthomeItem()
          
@@ -26,4 +40,5 @@ class AthomeDirSpider(scrapy.Spider):
         next_page = response.css('a.nextPage::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            sleep(randrange(5))
+            yield scrapy.Request(next_page, callback=self.parse, headers = self.request_header)
